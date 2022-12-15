18a50d04bcb1fa7d8098bc3f65478980caa1048e
gvbarker <gvbarker23@wabash.edu>
2022-12-15 05:24:47 -0500

    final push before demo, mostly functional, needs frontend magic

--- frontend-web/src/Components/BoxCategory.js
+++ frontend-web/src/Components/BoxCategory.js
@@ -11,17 +11,17 @@ function BoxCategory ( {
     
     const handleChange = (id) => {
         let temp = data.map((item) => {
-            if(id === item.id) {
+            if(id === item?.id) {
+                console.log(item)
                 return { ...item, isChecked: !item.isChecked }
             }
             return item
         });
         onChange(temp);
-        console.log(temp)
     };
-    console.log(data)
     let boxList = data.map((item,i) => {
-        return <CheckBox key={i} index={item.id} name={item.title} checked={item.isChecked} onChange={handleChange} />
+        
+        return <CheckBox key={i} index={item.id} name={item.name} checked={item.isChecked} onChange={()=>handleChange(item.id)} />
     })
     return( boxList )
 
